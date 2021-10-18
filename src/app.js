d3fcb898b23671e36c558f9001ded7c0815c1018
Şeyma İşyapar <seymaisyapar35@gmail.com>
2021-10-19 00:05:27 +0300

    18 Ekim

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+
+}
+
+console.log(dolarDun)
+
+const euroDun =11.2
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+console.log(konutKredileri)
+
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKrediler[i]+"</li>")
+}
+
+console.log("</ul>")
